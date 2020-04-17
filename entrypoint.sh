ad081ea6dbf4e05c3f63edf49855331133b7fd7a
Leo Leppänen <leo.leppanen@helsinki.fi>
2020-04-17 11:20:19 +0300

    Add more datasets to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 
 echo "Checking for data"
 wget -nc -nv -O /app/data/cphi.cache http://perfectlylegit.website/eunlg-data/cphi.cache
+wget -nc -nv -O /app/data/env.cache http://perfectlylegit.website/eunlg-data/env.cache
+wget -nc -nv -O /app/data/health_cost.cache http://perfectlylegit.website/eunlg-data/health_cost.cache
+wget -nc -nv -O /app/data/health_funding.cache http://perfectlylegit.website/eunlg-data/health_funding.cache
 exec "$@"
