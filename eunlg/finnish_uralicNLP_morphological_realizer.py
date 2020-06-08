7daf59450aa91f48b43298fe02046b44af66efc2
Leo Leppänen <leo.leppanen@helsinki.fi>
2020-06-08 14:36:44 +0300

    FIX: Incorrect Finnish morphological generation
    Fixes two bugs:
     1. Incorrectly picked plural forms of words over singulars (this will
        likely need to be re-adjusted in the future to "smartly" select
        between the two, the present fix just hard-codes the singular)
     2. The case attribute was ignored, and everything was generated in the
        genitive case.

--- eunlg/finnish_uralicNLP_morphological_realizer.py
+++ eunlg/finnish_uralicNLP_morphological_realizer.py
@@ -20,20 +20,26 @@ class FinnishUralicNLPMorphologicalRealizer(LanguageSpecificMorphologicalRealize
         if case is None:
             return slot.value
 
-        log.debug("Realizing {} to Finnish")
+        log.debug("Realizing {} to Finnish".format(slot.value))
 
         case = self.case_map.get(case.lower(), case.capitalize())
         log.debug("Normalized case {} to {}".format(slot.attributes.get("case"), case))
 
-        possible_analyses = uralicApi.analyze(slot.value, "fin")
+        possible_analyses = [
+            analysis[0]
+            for analysis in uralicApi.analyze(slot.value, "fin")
+            if "Nom" in analysis[0] and "Sg" in analysis[0]
+        ]
         log.debug("Identified {} possible analyses".format(len(possible_analyses)))
+        for analysis in possible_analyses:
+            log.debug("\t{}".format(analysis))
         if len(possible_analyses) == 0:
             log.warning(
                 "No valid morphological analysis for {}, unable to realize despite case attribute".format(slot.value)
             )
             return slot.value
 
-        analysis = possible_analyses[0][0]
+        analysis = possible_analyses[0]
         log.debug("Picked {} as the morphological analysis of {}".format(analysis, slot.value))
 
         # We only want to replace the last occurence of "Nom", as otherwise all parts of compound words, rather than
@@ -42,7 +48,7 @@ class FinnishUralicNLPMorphologicalRealizer(LanguageSpecificMorphologicalRealize
         # starts from the right, so we need to identify the correct instance of "Nom" with rfind() and then manually
         # fiddle with slices.
         gen_start_idx = analysis.rfind("Nom")
-        analysis = analysis[:gen_start_idx] + "Gen" + analysis[gen_start_idx + 4 :]  # 4 = 1 + len("Nom")
+        analysis = analysis[:gen_start_idx] + case + analysis[gen_start_idx + 4 :]  # 4 = 1 + len("Nom")
         log.debug("Modified analysis to {}".format(analysis))
 
         modified_value = uralicApi.generate(analysis, "fin")[0][0]
