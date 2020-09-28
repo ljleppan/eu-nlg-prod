9c0ca110b0eef825d0256c08f7950df5ede2e596
Leo Leppänen <leo.leppanen@helsinki.fi>
2020-09-28 17:08:28 +0300

    UTIL: Full DP only bulk generation helper
    Like bulk_gen.sh, but for generating docs with the "full" document
    planner only.

---
+++ eunlg/bulk_gen_full_only.sh
@@ -0,0 +1,7 @@
+python eunlg/bulk_generate.py -l en -o bulk_out/ -v full -d cphi -l AT BE BG CH CY CZ DE
+python eunlg/bulk_generate.py -l en -o bulk_out/ -v full -d cphi -l DK EE EL ES FI FR HR
+python eunlg/bulk_generate.py -l en -o bulk_out/ -v full -d cphi -l HU IE IS IT LT LU LV
+python eunlg/bulk_generate.py -l en -o bulk_out/ -v full -d cphi -l MK MT NL NO PL PT RO
+python eunlg/bulk_generate.py -l en -o bulk_out/ -v full -d cphi -l RS SE SI SK TR UK US all
+python eunlg/bulk_generate.py -l en -o bulk_out/ -v full -d health_cost
+python eunlg/bulk_generate.py -l en -o bulk_out/ -v full -d health_funding
