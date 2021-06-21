3d97ec2af3dd7069b5fdbaee50d2cbc715bfd584
Sardana Ivanova <sardana.ivanova@helsinki.fi>
2021-06-21 16:03:32 +0300

    cphi Russian translation

--- eunlg/resources/cphi_russian_resource.py
+++ eunlg/resources/cphi_russian_resource.py
@@ -6,89 +6,82 @@ from resources.tabular_data_resource import TabularDataResource
 TEMPLATES: str = """
 # PRESENT VALUE
 
-ru: [{time},] [в {location, case=loc},] {value_type} был(-а) {value} {unit}
-ru: [{time},] [в {location, case=loc},] it was {value} {unit} --- don't use this construction in Russian
-ru-head: в {location, case=loc}, в {time}, {value_type} был(-а) {value} {unit}
+ru: [в {time, case=loc2},] [в {location, case=loc2},] {value_type} был(-а) {value} {unit}
+ru-head: в {location, case=loc2}, в {time, case=loc2}, {value_type} был(-а) {value} {unit}
 | value_type = cphi:.*, value_type != .*:rank.*, value_type != .*:comp_.*
 
 # SINGLE VALUE COMP EU
 
-ru: [in {time},] [in {location},] the {value_type} was {value} {unit} more than the EU average
-ru: [in {time},] [in {location},] it was {value} {unit} more than the EU average
-ru-head: in {location}, in {time}, the {value_type} was {value} {unit} over EU average
+ru: [в {time, case=loc2},] [в {location, case=loc2},] {value_type} был(-а) {value} {unit} больше, чем в среднем по ЕС
+ru-head: в {location, case=loc2}, в {time, case=loc2}, {value_type} был(-а) {value} {unit} выше среднего по ЕС 
 | value_type = cphi:.*:comp_eu, value_type != .*:rank.*, value > 0
 
-ru: [in {time},] [in {location},] the {value_type} was {value, abs} {unit} less than the EU average
-ru: [in {time},] [in {location},] it was {value, abs} {unit} less than the EU average
-ru-head: in {location}, in {time}, {value_type} at {value, abs} {unit} below EU average
+ru: [в {time, case=loc2},] [в {location, case=loc2},] {value_type} был(-а) {value, abs} {unit} меньше, чем в среднем по ЕС
+ru-head: в {location, case=loc2}, в {time, case=loc2}, {value_type} на {value, abs} {unit} ниже среднего по ЕС 
 | value_type = cphi:.*:comp_eu, value_type != .*:rank.*, value < 0
 
-ru: [in {time},] [in {location},] the {value_type} was the same as the EU average
-ru: [in {time},] [in {location},] it was the same as the EU average
-ru-head: in {location}, in {time}, {value_type} tied with EU average
+ru: [в {time, case=loc2},] [в {location, case=loc2},] {value_type} был(-а) таким же, как в среднем по ЕС
+ru-head: в {location, case=loc2}, в {time, case=loc2}, {value_type} был(-а) таким же, как в среднем по ЕС
 | value_type = cphi:.*:comp_eu, value_type != .*:rank.*, value = 0.0
 
 # SINGLE VALUE COMP US
 
-ru: [in {time},] [in {location},] the {value_type} was {value} {unit} more than in US
-ru: [in {time},] [in {location},] it was {value} {unit} more than in US
-ru-head: in {location}, in {time}, {value_type} at {value} {unit} over US
+ru: [в {time, case=loc2},] [в {location, case=loc2},] {value_type} был(-а) {value} {unit} больше чем в США
+ru-head: в {location, case=loc2}, в {time, case=loc2}, {value_type} на {value} {unit} больше США
 | value_type = cphi:.*:comp_us, value_type != .*:rank.*, value > 0
 
-ru: [in {time},] [in {location},] the {value_type} was {value, abs} {unit} less than in US
-ru: [in {time},] [in {location},] it was {value, abs} {unit} less than in US
-ru-head: in {location}, in {time}, {value_type} at {value, abs} {unit} below US
+ru: [в {time, case=loc2},] [в {location, case=loc2},] {value_type} был(-а) {value, abs} {unit} меньше чем в США
+ru-head: in {location, case=loc2}, in {time, case=loc2}, {value_type} на {value, abs} {unit} меньше США
 | value_type = cphi:.*:comp_us, value_type != .*:rank.*, value < 0
 
-ru: [in {time},] [in {location},] the {value_type} was the same as in US
-ru: [in {time},] [in {location},] it was the same as in US
-ru-head: in {location}, in {time}, {value_type} tied with US
+ru: [в {time, case=loc2},] [в {location, case=loc2},] {value_type} был(-а) таким же, как в США 
+ru-head: в {location, case=loc2}, в {time, case=loc2}, {value_type} был(-а) таким же, как в США
 | value_type = cphi:.*:comp_us, value_type != .*:rank.*, value = 0.0
 
 # RANK
 
-ru: [in {time},] {location} had the {value, ord} highest {value_type} across the observed countries
-ru: [in {time},] {location} had the {value, ord} highest value for it across the observed countries
-ru-head: in {time}, {location, case=gen} {value, ord} {value_type} highest
+ru: [в {time, case=loc2},] {location, case=loc2} имел(-а) {value, ord} самый высокий {value_type} во всех наблюдаемых странах
+ru: [в {time, case=loc2},] {location, case=loc2} имел(-а) {value, ord} самый высокое значение во всех наблюдаемых странах
+ru-head: в {time, case=loc2}, {location, case=gen} {value, ord} {value_type} самое высокое
 | value_type = cphi:.*:rank.*, value_type != .*rank_reverse.*
 
 # RANK_REVERSE
 
-ru: [in {time},] {location} had the {value, ord} lowest {value_type} across the observed countries
-ru: [in {time}, ]{location} had the {value, ord} lowest value for it across the observed countries
-ru-head: in {time}, {location, case=gen} {value, ord} {value_type} lowest
+ru: [в {time, case=loc2},] {location, case=loc2} имел(-а) {value, ord} самый низкий {value_type} во всех наблюдаемых странах
+ru: [в {time, case=loc2}, ]{location, case=loc2} имел(-а) {value, ord} самое низкое для этого значение во всех наблюдаемых странах
+ru-head: в {time, case=loc2}, {location, case=gen} {value, ord} {value_type} самое низкое
 | value_type = cphi:.*:rank_reverse.*
 """
 
 INDEX_CATEGORIES: Dict[str, str] = {
-    "hicp2015": "harmonized consumer price index",  # harmonized consumer price index (2015=100)
-    "rt1": "monthly growth rate",  # growth rate on previous period (t/t-1)
-    "rt12": "yearly growth rate",  # growth rate (t/t-12)
-    "cp-hi00": "'all items'",
-    "cp-hi01": "'food and non-alcoholic beverages'",
-    "cp-hi02": "'alcoholic beverages and tobacco'",
-    "cp-hi03": "'clothing and footwear'",
-    "cp-hi04": "'housing, water, electricity, gas and other fuels'",
-    "cp-hi05": "'furnishings, household equipment and maintenance'",
-    "cp-hi06": "'health'",
-    "cp-hi07": "'transport'",
-    "cp-hi08": "'communication'",
-    "cp-hi09": "'recreation and culture'",
-    "cp-hi10": "'education'",
-    "cp-hi11": "'hotels, cafes and restaurants'",
-    "cp-hi12": "'miscellaneous goods and services'",
-    "cp-hi00xef": "'all items excluding energy, food, alcohol and tobacco'",
-    "cp-hi00xtb": "'all items excluding tobacco'",
-    "cp-hie": "'energy'",
-    "cp-hif": "'food'",
-    "cp-hifu": "'unprocessed food'",
-    "cp-hig": "'total goods'",
-    "cp-hiig": "'industrial goods'",
-    "cp-his": "'total services'",
-    "cp-hiigxe": "'non-energy industrial goods'",
-    "cp-hi00xe": "'all items excluding energy'",
-    "cp-hi00xefu": "'all items excluding energy and unprocessed food'",
-    "cp-hi00xes": "'all items excluding energy and seasonal food'",
+    "hicp2015": "согласованного индекса потребительских цен",  # harmonized consumer price index (2015=100)
+    "rt1": "ежемесячный темп роста",  # growth rate on previous period (t/t-1)
+    "rt12": "годовой темп роста",  # growth rate (t/t-12)
+    "cp-hi00": "'все элементы'",
+    "cp-hi01": "'еда и безалкогольные напитки'",
+    "cp-hi02": "'алкогольные напитки и табак'",
+    "cp-hi03": "'одежда и обувь'",
+    "cp-hi04": "'жилье, вода, электричество, газ и другие виды топлива'",
+    "cp-hi05": "'мебель, бытовая техника и обслуживание'",
+    "cp-hi06": "'здоровье'",
+    "cp-hi07": "'транспорт'",
+    "cp-hi08": "'общение'",
+    "cp-hi09": "'отдых и культура'",
+    "cp-hi10": "'образование'",
+    "cp-hi11": "'гостиницы, кафе и рестораны'",
+    "cp-hi12": "'разные товары и услуги'",
+    "cp-hi00xef": "'все товары, кроме энергии, еды, алкоголя и табака'",
+    "cp-hi00xtb": "'все товары, кроме табака'",
+    "cp-hie": "'энергия'",
+    "cp-hif": "'еда'",
+    "cp-hifu": "'необработанная еда'",
+    "cp-hig": "'всего товаров'",
+    "cp-hiig": "'промышленные товары'",
+    "cp-his": "'всего услуг'",
+    "cp-hiigxe": "'неэнергетические промышленные товары'",
+    "cp-hi00xe": "'все элементы, кроме энергии'",
+    "cp-hi00xefu": "'все товары, кроме энергоносителей и необработанных продуктов'",
+    "cp-hi00xes": "'все товары, кроме энергии и сезонной еды'",
 }
 
 
@@ -113,7 +106,7 @@ MAYBE_RANK_OR_COMP = ":?(rank|rank_reverse|comp_eu|comp_us)?"
 
 class RussianCphiRawRealizer(RegexRealizer):
     def __init__(self, registry):
-        super().__init__(registry, "ru", r"^cphi:([^:]*):([^:]*)" + MAYBE_RANK_OR_COMP + "$", "{} for the category {}")
+        super().__init__(registry, "ru", r"^cphi:([^:]*):([^:]*)" + MAYBE_RANK_OR_COMP + "$", "{} для категории {}")
 
 
 class RussianCphiChangeRealizer(RegexRealizer):
@@ -122,7 +115,7 @@ class RussianCphiChangeRealizer(RegexRealizer):
             registry,
             "ru",
             r"^cphi:([^:]*):([^:]*):(rt12?)" + MAYBE_RANK_OR_COMP + "$",
-            "{2} of the {0} for the category {1}",
+            "{2} {0} для категории {1}",
         )
 
 
@@ -132,7 +125,7 @@ class RussianUnitCphiPercentageRealizer(RegexRealizer):
             registry,
             "ru",
             r"^\[UNIT:cphi:.*\]$",
-            "percentage points",
+            "процентные пункты",
             slot_requirements=lambda slot: "rt1" in slot.value.split(":") or "rt12" in slot.value.split(":"),
         )
 
@@ -143,7 +136,7 @@ class RussianUnitCphiPointsRealizer(RegexRealizer):
             registry,
             "ru",
             r"^\[UNIT:cphi:.*\]$",
-            "points",
+            "пункты",
             slot_requirements=lambda slot: "rt1" not in slot.value.split(":") and "rt12" not in slot.value.split(":"),
         )
 
