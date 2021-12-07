9e893c197ed8729ef7a3222f8645f73e18a2a255
Leo Leppänen <leo.leppanen@helsinki.fi>
2021-12-07 18:08:17 +0200

    (re)download grammars in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ wget -nc -nv -O /app/data/cphi.cache http://perfectlylegit.website/eunlg-data/cp
 wget -nc -nv -O /app/data/env.cache http://perfectlylegit.website/eunlg-data/env.cache
 wget -nc -nv -O /app/data/health_cost.cache http://perfectlylegit.website/eunlg-data/health_cost.cache
 wget -nc -nv -O /app/data/health_funding.cache http://perfectlylegit.website/eunlg-data/health_funding.cache
+echo "Downloading grammars"
+python3 -m uralicNLP.download --language fin eng est
 exec "$@"
